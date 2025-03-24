0e8f2feda6f8710d6e5e70f6ff6a3ed95028530e
SamyMgi <samymegueni@gmail.com>
2025-03-24 18:45:04 +0100

    More comments for IM part

--- src/interaction_matrix.py
+++ src/interaction_matrix.py
@@ -23,7 +23,8 @@ class InteractionMatrix:
         """
         Create a dictionary mapping each sentences to their involved characters.
 
-        Returns: A dictionary where each key is a sentence index (int), and the value
+        Returns:
+            relations (dict): A dictionary where each key is a sentence index (int), and the value
                   is another dictionary containing:
                   - "sentence" (str): The original sentence.
                   - "characters" (list[str]): The characters involved in the sentence.
@@ -35,22 +36,23 @@ class InteractionMatrix:
         par_index = -1
         # CorefResolution only used for NER task
         cr = CorefResolution([], task="NER")
+        # Storing the characters involved in the previous sentence to regroup
         previous_person = []
+
         original_sents = list(self.original_doc.sents)
         resolved_sents = list(self.resolved_doc.sents)
-
+        # Sentences as str and removing empty sentences
         original_sents = [sent.text for sent in original_sents if (sent.text and not sent.text.isspace())]
         resolved_sents = [sent.text for sent in resolved_sents if (sent.text and not sent.text.isspace())]
 
-        print("\n-----INTERACTION MATRIX-----\n")
-
-        # Need to check same length for both texts
+        # Mapping each sentences to their associated characters
         for sent in resolved_sents:
             print("Original ver:", original_sents[sent_index])
-            print("Resolved ver:", sent, "\n")
+            print("Resolved ver:", sent)
             person = set(cr.get_person(sent))
-            print("Person on coref resolved :", person)
+            print("Person on coref resolved :", person, "\n")
             improved_person = set()
+            # Handling Firstname Lastname
             for pers in person:
                 if pers not in self.characters:
                     char = pers.split(" ")
@@ -59,7 +61,7 @@ class InteractionMatrix:
                             improved_person.add(split_char)
                 else:
                     improved_person.add(pers)
-
+            # Only considering sentences involving 2 characters or more for less processing
             if len(improved_person) > 1:
                 if improved_person == previous_person:
                     relations[par_index]["sent"] += original_sents[sent_index]
@@ -74,9 +76,6 @@ class InteractionMatrix:
 
         return relations
 
-    # Get the Interaction Matrix.
-    # Ex: ["Character A", "Character B"] = Value between -1 and 1
-    # 1 (Good relationship) / -1 (Bad relationship)
     def get_interaction_matrix(self):
         """
         Generates the Interaction Matrix using Zero-Shot Learning.
@@ -84,14 +83,15 @@ class InteractionMatrix:
         Each element quantifies the nature of the relationship between the corresponding characters,
         with values between -1 and +1, indicating negative or positive relationships respectively.
 
-        Returns: A DataFrame (pd.DataFrame) where the rows and columns represent characters, and each cell
+        Returns:
+            im_df (pd.DataFrame): A DataFrame where the rows and columns represent characters, and each cell
                 contains a score indicating the relationship between the characters.
                 Values range from -1 (bad relationship) to +1 (good relationship).
         """
         relation_dict = self._get_relation_dict()
         data = {char_1: {char_2: [] for char_2 in self.characters} for char_1 in self.characters}
 
-        df = pd.DataFrame(data)
+        im_df = pd.DataFrame(data)
 
         relation_extraction = pipeline("zero-shot-classification", model=self.model)
 
@@ -102,25 +102,27 @@ class InteractionMatrix:
             print("Involved :", chars)
             pairs = [[chars[i], chars[j]] for i in range(len(chars)) for j in range(i + 1, len(chars))]
 
+            # Considering 2 classes for ZSL: Allies and Enemies.
+            # Evaluating each pair and using names in the class labels for better accuracy.
             for cand in pairs:
                 cand_labels = [f"{cand[0]} and {cand[1]} are allies", f"{cand[0]} and {cand[1]} are enemies"]
                 output = relation_extraction(sent, candidate_labels=cand_labels)
-                # print(output)
+
                 if "allies" in output["labels"][0]:
                     relationship = 1
                 elif "enemies" in output["labels"][0]:
                     relationship = -1
                 else:
                     relationship = 0
-                # relationship = 1 if "allies" in output["labels"][0] else -1
-                # score = relationship * output["scores"][0]
+
                 score = relationship
                 if score != 0:
-                    df[cand[0]][cand[1]].append(score)
-                    df[cand[1]][cand[0]].append(score)
+                    im_df[cand[0]][cand[1]].append(score)
+                    im_df[cand[1]][cand[0]].append(score)
                 print(cand_labels, " = ", score, "\n")
 
-        df = df.apply(lambda column: column.map(lambda cell: np.nan if len(cell) == 0 else sum(cell) / len(cell)))
+        # Final Interaction Matrix considering the mean relationship score
+        im_df = im_df.apply(lambda column: column.map(lambda cell: np.nan if len(cell) == 0 else sum(cell) / len(cell)))
 
-        print(df)
-        return df
+        print(im_df)
+        return im_df
